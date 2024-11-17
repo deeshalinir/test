81b1b5658395c3e6696948e73fb044f98fd8d7c9
Deeshalini Ramgoolam <dr721@live.mdx.ac.uk>
2024-11-17 23:25:16 +0400

    created js and changed directory
    created a classes js file and changed the directory for images.

---
+++ cw_classes.js
@@ -0,0 +1,83 @@
+let classes = [
+    {
+        id: 1,
+        iconClass: "/frontend-cw/images/mathematics.png",
+        subject: "Mathematics",
+        location: "Hendon",
+        price: 23.00,
+        spaces: 5
+    },
+    {
+        id: 2,
+        iconClass: "/frontend-cw/images/coding.png",
+        subject: "Computer Science",
+        location: "Mont Rose",
+        price: 27.00,
+        spaces: 5
+        
+    },
+    {
+        id: 3,
+        iconClass: "./images/art.png",
+        subject: "Art",
+        location: "Hammersmith",
+        price: 25.00,
+        spaces: 5
+    },
+    {
+        id: 4,
+        iconClass: "./images/geography.png",
+        subject: "Geography",
+        location: "Collindate",
+        price: 17.00,
+        spaces: 5
+    },
+    {
+        id: 5,
+        iconClass: "./images/science.png",
+        subject: "Sciences",
+        location: "Edgeware",
+        price: 24.00,
+        spaces: 5    
+    },
+    {
+        id: 6,
+        iconClass: "./images/business.png",
+        subject: "Business Entrepreneuship",
+        location: "Hyde",
+        price: 29.00,
+        spaces: 5
+    },
+    {
+        id: 7,
+        iconClass: "./images/cycling.png",
+        subject: "Cycling Club",
+        location: "Petersborogh",
+        price: 10.00,
+        spaces: 5
+    },
+    {
+        id: 8,
+        iconClass: "./images/psycology.png",
+        subject: "Psychology",
+        location: "St Mary",
+        price: 33.00,
+        spaces: 5
+    },
+    {
+        id: 9,
+        iconClass: "./images/chinese.png",
+        subject: "Chinese",
+        location: "Florence",
+        price: 25.00,
+        spaces: 5
+    },
+    {
+        id: 10,
+        iconClass: "./images/debate.png",
+        subject: "Debate Club",
+        location: "Glebe",
+        price: 8.00,
+        spaces: 5
+    }
+]
